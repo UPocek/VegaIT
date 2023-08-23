bf92a5b8f24399ba8eb7b93212f42a8657073f33
Uros Pocek <uros.pocek@gmail.com>
2023-08-23 16:39:59 +0200

    feat: Added login and reqistration forms and logic

---
+++ timesheetfront/pages/index.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-multi-comp */
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import NavBar from '@/components/NavBar'
+// import { useState } from 'react'
+// import axios from 'axios';
+// import { baseUrl } from './_app';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+  // const [categories, setCategories] = useState([]);
+
+  // function getCategories() {
+  //   axios.get(`${baseUrl}/api/user`).then(result => setCategories(result.data)).catch(error => console.log(error));
+  // }
+
+  return (
+    <>
+      <Head>
+        <title>Uros Pocek</title>
+        <meta name="description" content="TimeSheet app for VegaIT" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <NavBar />
+        <MainArea />
+      </main>
+    </>
+  )
+}
+
+function MainArea() {
+  return (
+    <div></div>
+  );
+}
---
+++ timesheetfront/pages/login.jsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer";
+import LoginForm from "@/components/LoginForm";
+import MinimalNav from "@/components/MinimalNav";
+
+export default function Login() {
+    return (
+        <>
+            <MinimalNav />
+            <LoginForm />
+            <Footer />
+        </>)
+}
